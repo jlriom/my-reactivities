e5436d662d8f4039289674cd1a231415def0a03a
Jose Luis del Rio <jlriom@hotmail.es>
2025-12-24 09:36:37 +0100

    init

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController(AppDbContext context) : BaseApiController
+    {
+        private readonly AppDbContext _context  = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            
+            if (activity == null) return NotFound();
+
+            return activity;
+        }
+
+    }
+}
