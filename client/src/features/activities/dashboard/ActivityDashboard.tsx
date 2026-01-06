59ccb1cfbe198dae6be20abeefc6bf571f08b307
Jose Luis del Rio <jlriom@hotmail.es>
2026-01-06 22:02:56 +0100

    client - activities

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,56 @@
+import { Grid } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+type Props = {
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  selectedActivity?: Activity;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  editMode: boolean;
+  submitForm: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+};
+
+export default function ActivityDashBoard({
+  activities,
+  selectActivity,
+  cancelSelectActivity,
+  selectedActivity,
+  openForm,
+  closeForm,
+  editMode,
+  submitForm,
+  deleteActivity,
+}: Props) {
+  return (
+    <Grid container spacing={3}>
+      <Grid size={7}>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+        ></ActivityList>
+      </Grid>
+      <Grid size={5}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
+          ></ActivityDetails>
+        )}
+        {editMode && (
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+            submitForm={submitForm}
+          ></ActivityForm>
+        )}
+      </Grid>
+    </Grid>
+  );
+}
